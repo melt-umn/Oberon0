b12155b206335ceca72321d1b5702b66d9ed8eab
Eric Van Wyk <evw@umn.edu>
2020-02-18 13:21:25 -0600

    Silver no longer allows whitespace in regex literals

--- edu.umn.cs.melt.Oberon0/core/concreteSyntax/Terminals.sv
+++ edu.umn.cs.melt.Oberon0/core/concreteSyntax/Terminals.sv
@@ -4,10 +4,14 @@ lexer class IDENTIFIER;
 lexer class KEYWORD dominates IDENTIFIER;
 
 ignore terminal WhiteSpace /[\r\n\t\ ]+/;
-ignore terminal Comment /\(\*             -- Opening lparen-star
-                          ([^\*]+|        -- Anything not including a star
-                           (\*+[^\*\)]))* -- stars, followed by non-rparen
-                         \*+\)/;          -- Closing stars-rparen
+
+{-
+Comment /\(\*             -- Opening lparen-star
+          ([^\*]+|        -- Anything not including a star
+          (\*+[^\*\)]))* -- stars, followed by non-rparen
+         \*+\)/;          -- Closing stars-rparen
+-}
+ignore terminal Comment /\(\*([^\*]+|(\*+[^\*\)]))*\*+\)/;
 
 terminal Id_t           /[A-Za-z][A-Za-z0-9]*/  lexer classes {IDENTIFIER};
 terminal Num_t          /[0-9]+/;
