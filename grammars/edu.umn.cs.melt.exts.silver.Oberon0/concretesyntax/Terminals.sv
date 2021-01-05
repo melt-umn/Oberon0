8662dc663bef8f4f1eee475e7d5ef912b40e6e6a
Lucas Kramer <krame505@umn.edu>
2021-01-04 23:29:44 -0600

    Remove superfluous imports

--- grammars/edu.umn.cs.melt.exts.silver.Oberon0/concretesyntax/Terminals.sv
+++ grammars/edu.umn.cs.melt.exts.silver.Oberon0/concretesyntax/Terminals.sv
@@ -1,8 +1,5 @@
 grammar edu:umn:cs:melt:exts:silver:Oberon0:concretesyntax;
 
-imports silver:compiler:definition:regex as silver;
-imports silver:reflect:concretesyntax;
-
 -- Terminal definitions for quote productions
 marking terminal Oberon0Stmt_t 'Oberon0_Stmt'  lexer classes {silver:KEYWORD, silver:RESERVED};
 marking terminal Oberon0Expr_t 'Oberon0_Expr'  lexer classes {silver:KEYWORD, silver:RESERVED};
