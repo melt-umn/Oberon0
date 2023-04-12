5df4452203801c5d60aa5c58f9d9f911cdab5b46
alexandra-hanson <alexandra.rose.hanson@gmail.com>
2023-04-12 17:51:40 -0500

    Get semantic highlighting working for oberon0 lsp

--- language-server/langserver/src/main/java/edu/umn/cs/melt/oberon0/langserver/Oberon0LanguageServer.java
+++ language-server/langserver/src/main/java/edu/umn/cs/melt/oberon0/langserver/Oberon0LanguageServer.java
@@ -1,10 +1,24 @@
 package edu.umn.cs.melt.oberon0.langserver;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.MessageParams;
+import org.eclipse.lsp4j.MessageType;
+import org.eclipse.lsp4j.SemanticTokensLegend;
+import org.eclipse.lsp4j.SemanticTokensServerFull;
+import org.eclipse.lsp4j.SemanticTokensWithRegistrationOptions;
 import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.TextDocumentSyncKind;
+import org.eclipse.lsp4j.WorkspaceFolder;
 
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.LanguageClientAware;
@@ -12,6 +26,10 @@ import org.eclipse.lsp4j.services.LanguageServer;
 import org.eclipse.lsp4j.services.TextDocumentService;
 import org.eclipse.lsp4j.services.WorkspaceService;
 
+import com.google.gson.JsonObject;
+
+import edu.umn.cs.melt.lsp4jutil.Util;
+import edu.umn.cs.melt.Oberon0.core.concreteSyntax.NModule_c;
 
 public class Oberon0LanguageServer implements LanguageServer, LanguageClientAware {
 
@@ -23,13 +41,81 @@ public class Oberon0LanguageServer implements LanguageServer, LanguageClientAwar
   }
 
   @Override
-	public CompletableFuture<InitializeResult> initialize(InitializeParams params) {
+	public CompletableFuture<InitializeResult> initialize(InitializeParams initializeParams) {
 		System.err.println("Initializing Oberon0 language server");
 
+    // Initialize the runtime
+    common.Util.init();
+
+    // Get the initialization options
+    String compilerJar = "", parserName = "";
+    try {
+        JsonObject initOptions = (JsonObject)initializeParams.getInitializationOptions();
+        if (initOptions != null) {
+            if (initOptions.has("compilerJar")) {
+                compilerJar = initOptions.get("compilerJar").getAsString();
+            }
+            if (initOptions.has("parserName")) {
+                parserName = initOptions.get("parserName").getAsString();
+            }
+        }
+    } catch(ClassCastException e) {
+        System.err.println("Got unexpected init options: " + initializeParams.getInitializationOptions());
+    }
+    if (parserName.isEmpty()) {
+        parserName = "edu:umn:cs:melt:Oberon0:components:L4:parse";
+    }
+
+    // Initialize grammars from the specified compiler jar (or default classloader)
+    ClassLoader loader = ClassLoader.getSystemClassLoader();
+
+    if (!compilerJar.isEmpty()) {
+      List<WorkspaceFolder> workspaceFolders = initializeParams.getWorkspaceFolders();
+      Path jarPath;
+      try {
+          // Compute the absolute jar path based on the workspace folder, if a relative path is specified.
+          jarPath = workspaceFolders != null && workspaceFolders.size() > 0?
+              Paths.get(new URI(initializeParams.getWorkspaceFolders().get(0).getUri()).getPath()).resolve(compilerJar) :
+              Paths.get(compilerJar);
+      } catch (URISyntaxException e) {
+        throw new RuntimeException(e);
+      }
+      loader = Util.getJarClassLoader(jarPath);
+    }
+
+    // Load the specified parser
+    boolean loadedParser = false;
+    try {
+        service.setParserFactory(Util.loadCopperParserFactory(loader, parserName, NModule_c.class));
+        loadedParser = true;
+    } catch (SecurityException | ReflectiveOperationException e) {
+        client.showMessage(new MessageParams(MessageType.Error, "Error loading parser " + parserName + " from jar: " + e.toString()));
+    }
+
+    // Set up the Oberon0 grammars folder?
+    Path oberon0Grammars;
+    try {
+        oberon0Grammars = Files.createTempDirectory("oberon0_grammars");
+        Util.copyFromJar(getClass(), "grammars/", oberon0Grammars);
+    } catch (IOException | URISyntaxException e) {
+        throw new RuntimeException(e);
+    }
+    service.setOberon0GrammarsPath(oberon0Grammars);
+
+    // Set the capabilities of the LS to inform the client.
     ServerCapabilities capabilities = new ServerCapabilities();
+    capabilities.setTextDocumentSync(TextDocumentSyncKind.Full);
+    if (loadedParser) {
+        capabilities.setSemanticTokensProvider(
+            new SemanticTokensWithRegistrationOptions(
+                new SemanticTokensLegend(
+                    Oberon0LanguageService.tokenTypes, Oberon0LanguageService.tokenModifiers),
+                new SemanticTokensServerFull(false), false));
+    }
 
     final InitializeResult initializeResult = new InitializeResult(capabilities);
     return CompletableFuture.supplyAsync(()->initializeResult);
+
 	}
 
 	@Override
--- language-server/langserver/src/main/java/edu/umn/cs/melt/oberon0/langserver/Oberon0LanguageService.java
+++ language-server/langserver/src/main/java/edu/umn/cs/melt/oberon0/langserver/Oberon0LanguageService.java
@@ -1,26 +1,77 @@
 package edu.umn.cs.melt.oberon0.langserver;
 
-import org.eclipse.lsp4j.ConfigurationParams;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.nio.file.Path;
+import java.util.function.Supplier;
+
 import org.eclipse.lsp4j.DidOpenTextDocumentParams;
 import org.eclipse.lsp4j.DidChangeConfigurationParams;
 import org.eclipse.lsp4j.DidChangeTextDocumentParams;
 import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
 import org.eclipse.lsp4j.DidCloseTextDocumentParams;
 import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.SemanticTokens;
+import org.eclipse.lsp4j.SemanticTokensParams;
+import org.eclipse.lsp4j.WorkspaceFolder;
+import org.eclipse.lsp4j.jsonrpc.CompletableFutures;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.TextDocumentService;
 import org.eclipse.lsp4j.services.WorkspaceService;
 
+import common.DecoratedNode;
+import common.SilverCopperParser;
+import edu.umn.cs.melt.lsp4jutil.CopperParserNodeFactory;
+import edu.umn.cs.melt.lsp4jutil.CopperSemanticTokenEncoder;
+import edu.umn.cs.melt.Oberon0.core.concreteSyntax.NModule_c;
+
 public class Oberon0LanguageService implements TextDocumentService, WorkspaceService {
 
   private LanguageClient client;
+  private List<WorkspaceFolder> folders;
+  private Map<String, String> fileContents = new HashMap<>();
+  private Map<String, Integer> fileVersions = new HashMap<>();
+  private Map<String, Integer> savedVersions = new HashMap<>();
+  private String generated;
+
+  private String oberon0Grammars = null;
+
+  public Oberon0LanguageService() {
+      try {
+          generated = Files.createTempDirectory("generated").toString() + "/";
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
 
   public void setClient(LanguageClient client) {
     this.client = client;
   }
 
+  public void setOberon0GrammarsPath(Path path) {
+    this.oberon0Grammars = path.toString() + "/";
+  }
+
+  public void setWorkspaceFolders(List<WorkspaceFolder> folders) {
+      this.folders = folders;
+      //refreshWorkspace();
+  }
+
 	@Override
 	public void didOpen(DidOpenTextDocumentParams params) {
+      String uri = params.getTextDocument().getUri();
+      fileContents.put(uri, params.getTextDocument().getText());
+      fileVersions.put(uri, params.getTextDocument().getVersion());
+      savedVersions.put(uri, params.getTextDocument().getVersion());
+      //triggerBuild(false);
 	}
 
 	@Override
@@ -43,6 +94,44 @@ public class Oberon0LanguageService implements TextDocumentService, WorkspaceSer
   @Override
   public void didChangeWatchedFiles(DidChangeWatchedFilesParams params) {
 
+  }
+
+  public static final List<String> tokenTypes = Arrays.asList(new String[] {
+      "namespace", "type", "interface", "class", "typeParameter", "parameter", "variable", "function",
+      "keyword", "modifier", "comment", "string", "number", "regexp", "operator", "struct"
+  });
+  public static final List<String> tokenModifiers = Arrays.asList(new String[] {
+      "declaration", "definition", "documentation", "defaultLibrary", "modification"
+  });
+
+  private CopperSemanticTokenEncoder semanticTokenEncoder = null;
+  private CopperParserNodeFactory parserFn = null;
+  public void setParserFactory(Supplier<SilverCopperParser<NModule_c>> parserFactory) {
+      semanticTokenEncoder = new CopperSemanticTokenEncoder(parserFactory, tokenTypes, tokenModifiers);
+      parserFn = new CopperParserNodeFactory(parserFactory);
+  }
+
+  @Override
+  public CompletableFuture<SemanticTokens> semanticTokensFull(SemanticTokensParams params) {
+      if (semanticTokenEncoder == null) {
+          throw new IllegalStateException("Semantic tokens requested when parser has not been initialized");
+      }
+
+      String uri = params.getTextDocument().getUri();
+
+      return CompletableFutures.computeAsync((cancelChecker) -> {
+          List<Integer> tokens;
+          int requestVersion;
+          do {
+              requestVersion = fileVersions.get(uri);
+              if (fileContents.containsKey(uri)) {
+                  tokens = semanticTokenEncoder.parseTokens(fileContents.get(uri));
+              } else {
+                  tokens = new ArrayList<Integer>();
+              }
+          } while (requestVersion != fileVersions.get(uri));
+          return new SemanticTokens(tokens);
+      });
   }
 
 }
