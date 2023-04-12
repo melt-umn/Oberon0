070845ec9707ee87a6cd358b2b1c0ec4d201d561
alexandra-hanson <alexandra.rose.hanson@gmail.com>
2023-04-12 17:51:25 -0500

    Use correct jar and parser for Oberon0

--- language-server/build.sh
+++ language-server/build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-#TO DO
+mvn install:install-file -Dfile=../oberon0_a5.jar -DgroupId=edu.umn.cs.melt -DartifactId=oberon0 -Dversion=0.4.4-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
+
 
 mvn package
