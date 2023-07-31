00ae8d40801e25731970c3fe1d862ba715c441f4
alexandra-hanson <alexandra.rose.hanson@gmail.com>
2023-07-31 10:46:42 -0500

    Fix layout for condition table

--- grammars/edu.umn.cs.melt.exts.Oberon0.tables/concreteSyntax/Tables.sv
+++ grammars/edu.umn.cs.melt.exts.Oberon0.tables/concreteSyntax/Tables.sv
@@ -1,4 +1,4 @@
---grammar edu:umn:cs:melt:exts:Oberon0:concreteSyntax;
+grammar edu:umn:cs:melt:exts:Oberon0:tables:concreteSyntax;
 
 imports edu:umn:cs:melt:Oberon0:core:concreteSyntax as cnc;
 
@@ -7,7 +7,7 @@ imports edu:umn:cs:melt:exts:Oberon0:tables:abstractSyntax;
 imports silver:langutil;
 
 -- Terminals
-marking terminal Table_t 'TABLE' lexer classes {cnc:KEYWORD};
+marking terminal Table_t 'TABLE' lexer classes { cnc:KEYWORD };
 
 terminal LCurly_t '{'; -- action { context = head(context) :: context; };
 terminal RCurly_t '}'; -- action { context = tail(context); };
@@ -17,27 +17,35 @@ terminal TrueTV_t   'T';
 terminal FalseTV_t  'F';
 terminal StarTV_t   '*';
 
+ignore terminal Spaces_t /[\t ]+/;
+
 disambiguate NewLine_t, cnc:WhiteSpace
 {
   pluck NewLine_t;
 }
 
+disambiguate Spaces_t, cnc:WhiteSpace
+{
+  pluck Spaces_t;
+}
+
 -- Nonterminals
 nonterminal TableRows_c with ast<TableRows>, location;
 nonterminal TableRow_c  with ast<TableRow>, location;
-nonterminal TruthValueList_c with ast<TruthFlagList>, location;
+nonterminal TruthValueList_c  with ast<TruthFlagList>, location;
 nonterminal TruthValue_c with ast<TruthFlag>, location;
 
 -- Productions
 concrete production table_c
 top::cnc:Expr_c ::= 'TABLE' '{' trows::TableRows_c '}'
+layout { Spaces_t }
 {
   top.ast = table(trows.ast, location=top.location);
 }
 
 concrete production tableRowSnoc_c
 top::TableRows_c ::= trowstail::TableRows_c  n::NewLine_t  trow::TableRow_c
-layout { cnc:WhiteSpace }
+layout { Spaces_t }
 {
   top.ast = tableRowSnoc(trowstail.ast, trow.ast, location=top.location);
 }
